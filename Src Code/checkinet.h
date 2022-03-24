ae7ad0f57659753bffa4c0d5634bbda77b74de1e
Buzzer Felix <101106797+BuzzerFelix@users.noreply.github.com>
2022-03-24 18:25:45 +0200

    Check Internet (Wi-Fi or Ethernet, Nevermind)

--- Src Code/checkinet.h
+++ Src Code/checkinet.h
@@ -3,8 +3,8 @@
 #include "includes.h"
 using namespace std;
 
-void CheckInternet() {
-    bool check = InternetCheckConnectionA((LPCSTR)"https://www.google.com/", FLAG_ICC_FORCE_CONNECTION, 0);
+void CheckInternet(LPCSTR website) {
+    bool check = InternetCheckConnectionA((LPCSTR)website, FLAG_ICC_FORCE_CONNECTION, 0);
     if (check) {
         cout << xorstr("You are Connected to Internet...");
     }
